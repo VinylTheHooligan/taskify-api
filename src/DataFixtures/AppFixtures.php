de2fe5c84e6c7626d2411a03e620fa686b6e2993
VinylTheHooligan <vinyltheduster@gmail.com>
2026-03-15 11:31:25 +0100

    feat : Installation of Zenstruck, setup of user-related fixtures

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,6 +2,7 @@
 
 namespace App\DataFixtures;
 
+use App\Factory\UserFactory;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
@@ -9,8 +10,12 @@ class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        
+        UserFactory::createOne([
+            'email' => 'admin@admin.fr',
+            'roles' => ['ROLE_ADMIN']
+        ]);
+        UserFactory::createMany(100);
 
         $manager->flush();
     }
